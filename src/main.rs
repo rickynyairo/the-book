9381b2cd4e40df99c4694c35b68a50b05c1406a1
Ricky Nyairo <rickynyairo@gmail.com>
2023-05-17 21:33:18 +0300

    slices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+// use crate reference_and_borrowing::reference_and_borrowin;
+// mod ownership;
+
 fn main() {
+    let s = String::from("Hello world!");
     println!("Hello, world!");
+    println!("First space is at => {}", first_word(&s));
+    let hello = &s[0..5];
+    let world = &s[6..11];
+    println!("{} {}!", hello, world);
+    println!("First word is => {}", first_word_slice(&s))
+}
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+fn first_word_slice(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
---
+++ src/reference_and_borrowing.rs
@@ -0,0 +1,12 @@
+pub fn reference_and_borrowin() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize { // s is a reference to a String
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what
+// it refers to, it is not dropped.
